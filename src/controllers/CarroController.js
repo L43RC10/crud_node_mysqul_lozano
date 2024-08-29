11ec904f3ab57ee98ac8b664e28ae7d039fc1306
Laercio Pereira <laercio3714@gmail.com>
2024-08-29 15:38:05 -0300

    inserindo dados no db

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -26,5 +26,25 @@ module.exports = {
         }
 
         res.json(json);
+    },
+
+    inserir: async (req, res) => {
+        let json = {error: '', result:{}};
+
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+
+        if(modelo && placa){
+            let carroCodigo = await CarroService.inserir(modelo, placa);
+            json.results = {
+                codigo: carroCodigo,
+                modelo,
+                placa
+            };
+        } else {
+            json.error = 'Campos não enviados';
+        }
+
+        res.json(json);
     }
 }
